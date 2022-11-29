c4ca63eb164cd423d1057a5dd0f567f09e716b50
AndreMigz <andre@bootyard.com>
2022-11-29 15:02:55 +0800

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  def after_sign_up_path_for(resource_or_scope)
+    root_path
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            flash[:success] = 'Post successfully created'
+            redirect_to posts_path
+        else
+            flash[:error] = 'Something went wrong'
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :content)
+        end
+end
